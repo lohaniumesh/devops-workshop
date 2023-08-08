7c7487001f6b6d79f56e358431fb12e30c982bdf
Umesh Lohani <lohaniumesh@gmail.com>
2023-08-08 09:20:56 -0500

    updated terraform file

--- terraform_code_upd/V1-EC2.tf
+++ terraform_code_upd/V1-EC2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "demo-server" {
     ami = "ami-0f34c5ae932e6f0e4"
     instance_type = "t2.micro"
     key_name = "DevOps_Project"
-    security_groups = [aws_security_group.allow_ssh]
+    security_groups = ["allow_ssh"]
 }
 
 resource "aws_security_group" "allow_ssh" {
