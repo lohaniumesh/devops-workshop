4534b3123c29759c3d2b557e9ba1fe8743e62558
Umesh Lohani <lohaniumesh@gmail.com>
2023-08-14 10:13:58 -0500

    added terraform files for K8 cluster

--- terraform_code_upd/vpc/V1-EC2.tf
+++ terraform_code_upd/vpc/V1-EC2.tf
@@ -28,6 +28,14 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "Custom port to acess Jenkins"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -91,4 +99,16 @@ resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
 resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
   subnet_id = aws_subnet.dpp-public-subnet-02.id
   route_table_id = aws_route_table.dpp-public-rt.id
+}
+
+module "sgs" {
+  source = "../sg_eks"
+  vpc_id     =     aws_vpc.dpp-vpc.id
+}
+
+module "eks" {
+  source = "../eks"
+  vpc_id     =     aws_vpc.dpp-vpc.id
+  subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
+  sg_ids = module.sgs.security_group_public
 }
